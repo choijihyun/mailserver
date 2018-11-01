a59da2679a89f7ccc632b9de21ef835b8285fab4
yeongjoon <go1217jo@naver.com>
2018-11-01 11:41:26 +0900

    test success

--- MailServer/src/Main.java
+++ MailServer/src/Main.java
@@ -2,8 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         SMTPSender sender = new SMTPSender();
-        String id = "", password = "";
-        if(sender.sendMail(id, password,"go1217jo@gmail.com", "go1217jo@naver.com", "메일 전송 시스템 확인"))
+        String id = "구글 아이디", password = "구글 비밀번호";
+        String fromEmail = "";
+        String toEmail = "";
+        if(sender.sendMail(id, password, fromEmail, toEmail, "메일 전송 시스템 확인"))
             System.out.println("메일 전송 성공!");
         else
             System.out.println("메일 전송 실패!");
