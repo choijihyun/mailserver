5750fa8a41f9ead312a83b193f585fb4285b66c6
yeongjoon <go1217jo@naver.com>
2018-11-01 11:38:10 +0900

    feat : ssl connection

--- MailServer/src/Main.java
+++ MailServer/src/Main.java
@@ -2,7 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         SMTPSender sender = new SMTPSender();
-        if(sender.sendMail("go1217jo@naver.com", "go1217jo@gmail.com", "메일 전송 시스템 확인"))
+        String id = "", password = "";
+        if(sender.sendMail(id, password,"go1217jo@gmail.com", "go1217jo@naver.com", "메일 전송 시스템 확인"))
             System.out.println("메일 전송 성공!");
         else
             System.out.println("메일 전송 실패!");
--- MailServer/src/SMTPSender.java
+++ MailServer/src/SMTPSender.java
@@ -1,5 +1,9 @@
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 import java.net.*;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 public class SMTPSender {
     /* nslookup
@@ -7,31 +11,36 @@ public class SMTPSender {
        > [도메인 명] 으로 메일서버 주소를 확인할 수 있다.*/
     private String myMXAddress = "alt3.aspmx.l.google.com";
     private String domainName = "google";
-    private Socket socket;
+
+    private SSLSocket socket;
     // 서버 응답을 읽어오는 객체
     private BufferedReader br;
     // 서버에 요구를 보내는 객체
     private PrintWriter pw;
     private String statusCode;
 
+    private String id;
+    private String password;
+
     // 자신의 SMTP 서버에 연결
-    protected boolean connectMyServer() throws IOException {
+    protected boolean connectServer(String user, String password) throws IOException {
+        String line = "";
         // SMTP 메일 서버에 소켓 연결
-        socket = new Socket(myMXAddress, 25);
+        socket = (SSLSocket) ((SSLSocketFactory) SSLSocketFactory.getDefault())
+                .createSocket(InetAddress.getByName("smtp.gmail.com"), 465);
+        if(socket == null)
+            return false;
+
+        this.id = Base64.getEncoder().encodeToString(user.getBytes(StandardCharsets.UTF_8));
+        this.password = Base64.getEncoder().encodeToString(password.getBytes(StandardCharsets.UTF_8));
 
         // 생성된 소켓으로, 서버와 stream 통신을 할 수 있는 객체를 생성
         br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         pw = new PrintWriter(socket.getOutputStream(), true);
         System.out.println("서버 연결됨");
+        System.out.println("response : " + br.readLine());
 
-        String line = br.readLine();
-        System.out.println("response : " + line);
-        statusCode = line.substring(0,3);
-        if(statusCode.equals("220")) {
-            System.out.println("서버 응답 성공");
-            return true;
-        }
-        return false;
+        return true;
     }
 
     protected boolean sendCommand(String command, String param, String responseCode) throws IOException {
@@ -46,29 +55,37 @@ public class SMTPSender {
         return true;
     }
 
-    public boolean sendMail(String from, String to, String content) {
+    public boolean sendMail(String userID, String passwd, String from, String to, String content) {
         String line = "";
         try {
             // 본인 메일 서버 연결
-            connectMyServer();
+            if(!connectServer(userID, passwd))
+                return false;
         }catch (IOException e) {
             e.printStackTrace();
             System.out.println("본인 메일 서버 연결 실패");
             return false;
         }
         try {
+            /*
             // HELO 명령 전송, 응답코드 250 확인
-            if(!sendCommand("HELO ", domainName, "250"))
-                return false;
+            if(!sendCommand("HELO ", localhost.getHostName(), "250"))
+                return false;*/
 
             // EHLO 명령 전송, 응답코드 250 확인
-            if(!sendCommand("EHLO ", domainName, "250"))
+            if(!sendCommand("EHLO ", "smtp.gmail.com", "250"))
                 return false;
 
             pw.println("AUTH LOGIN");
-            // http://www.utilities-online.info/base64/#.W9pE6T5bRPY
-            pw.println("base64로 암호화된 아이디");
-            pw.println("base64로 암호화된 비밀번호");
+            line = br.readLine();
+            while(!line.substring(0,3).equals("334")) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+            pw.println(this.id);
+            System.out.println("response : " + br.readLine());
+            pw.println(this.password);
+            System.out.println("response : " + br.readLine());
 
             // MAIL FROM 명령 전송, 응답코드 250 확인
             if(!sendCommand("MAIL FROM: ", "<" + from + ">", "250"))
@@ -83,6 +100,9 @@ public class SMTPSender {
                 return false;
 
             // 본문 전송
+            pw.println("From: Test <" + from + ">");
+            pw.println("To: Me <" + to + ">");
+            pw.println("Subject: Testing email from telnet");
             pw.println(content);
             // 본문 전송 마무리
             if(!sendCommand(".", "", "250"))
@@ -94,7 +114,7 @@ public class SMTPSender {
             return false;
         }finally {
             try {
-                pw.println("quit");
+                pw.println("QUIT");
                 br.close();
                 pw.close();
                 socket.close();
