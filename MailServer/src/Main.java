26ad2e04cf58626aac316f091043e6241b5bd891
정주호 <johozang@gmail.com>
2018-11-06 23:55:52 +0900

    1. get mail using pop  2. smpt and pop are connneted to main

--- MailServer/src/Main.java
+++ MailServer/src/Main.java
@@ -1,13 +1,80 @@
-public class Main {
+package networkProject;
 
-    public static void main(String[] args) {
-        SMTPSender sender = new SMTPSender();
-        String id = "아이디", password = "비밀번호";
-        String fromEmail = "전송자";
-        String toEmail = "수신자";
-        if(sender.sendMail(id, password, fromEmail, toEmail, "네이버 메일에서 다음 메일로 전송\n"))
-            System.out.println("메일 전송 성공!");
-        else
-            System.out.println("메일 전송 실패!");
-    }
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		while (true) {
+			System.out.println("=============================");
+			System.out.println("\tMAIL SERVICE\t");
+			System.out.println("=============================");
+
+			System.out.println("1. SEND MAIL");
+			System.out.println("2. READ MAIL");
+			System.out.println("3. CLOSE");
+
+			System.out.printf("ENTER THE NUMBER : ");
+			String type;
+			int number;
+
+			type = input.next();
+			number = type.charAt(0)-'0';
+			input.nextLine();
+
+			switch (number) {
+			case 1:
+				SMTPSender sender = new SMTPSender();
+				String MXAccount, MXPassword;
+				String toEmail, Subject, Content;
+				
+				System.out.println("User Account : ");
+				MXAccount = input.next();
+				System.out.println("User Password : ");
+				MXPassword = input.next();
+				System.out.println("To : ");
+				toEmail = input.next();
+				System.out.println("Subject : ");
+				Subject = input.next();
+				System.out.println("Content : ");
+				Content = input.next();
+				if (sender.sendMail(MXAccount, MXPassword, toEmail, Subject, Content))
+					System.out.println("Success to send mail!");
+				else
+					System.out.println("Fail to send mail!");
+
+				break;
+			case 2:
+				PopClient client = new PopClient();
+				client.popPort = 995;
+
+				System.out.println("User Account : ");
+				client.popAccount = input.next();
+				System.out.println("User Password : ");
+				client.popPassword = input.next();
+
+				int index = client.popAccount.indexOf("@");
+				String mail = client.popAccount.substring(index + 1);
+				client.popServer = "pop." + mail;
+
+				System.out.println(client.popServer + "\n");
+				System.out.println(client.popAccount + "\n");
+
+				try {
+					client.read();
+					System.out.println("Reading Success");
+				} catch (Exception e) {
+					System.out.println("Reading Fail");
+				}
+				break;
+			case 3:
+				System.exit(0);
+				return;
+			default:
+				System.out.println("PLEASE REENTER");
+				break;
+			}
+
+		}
+	}
 }
--- MailServer/src/SMTPSender.java
+++ MailServer/src/SMTPSender.java
@@ -1,3 +1,5 @@
+package networkProject;
+
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import java.net.*;
@@ -6,23 +8,24 @@ import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 public class SMTPSender {
-    /* nslookup
-       > set type=mx
-       > [도메인 명] 으로 메일서버 주소를 확인할 수 있다.*/
-    private String domainName = "google";
 
-    private SSLSocket socket;
-    // 서버 응답을 읽어오는 객체
-    private BufferedReader br;
-    // 서버에 요구를 보내는 객체
-    private PrintWriter pw;
-    private String statusCode;
+	/* nslookup > set type=mx > [domain name] 
+	 * 
+	 * we can check mailserver address with that one*/
+	private String domainName = "google";
 
-    private String id;
-    private String password;
+	private SSLSocket socket;
+	//read the response from server
+	private BufferedReader br;
+	//send the request to server
+	private PrintWriter pw;
+	private String statusCode;
 
-    // 자신의 SMTP 서버에 연결
-    protected boolean connectServer(String domainName, int socketNumber) throws IOException {
+	private String id;
+	private String password;
+
+	// connect to own SMTP server
+	protected boolean connectServer(String domainName, int socketNumber) throws IOException {
         String line = "";
         // SMTP 메일 서버에 소켓 연결
         socket = (SSLSocket) ((SSLSocketFactory) SSLSocketFactory.getDefault())
@@ -39,108 +42,114 @@ public class SMTPSender {
         return true;
     }
 
-    protected boolean sendCommand(String command, String param, String responseCode) throws IOException {
-        String line = "", code;
-        pw.println(command + param);
-        line = br.readLine();
-        System.out.println("response : " + line);
-        code = line.substring(0,3);
-        if(!code.equals(responseCode))
-            return false;
+	// confirm the code with responseCode
+	protected boolean sendCommand(String command, String param, String responseCode) throws IOException {
+		String line = "", code;
+		pw.println(command + param);
+		line = br.readLine();
+		System.out.println("response : " + line);
+		code = line.substring(0, 3);
+		if (!code.equals(responseCode))
+			return false;
 
-        return true;
-    }
-
-    // 특수한 도메인네임에 대한 예외처리와 메일 서버 주소로 변환하여 반환
+		return true;
+	}
+	
+	 // 특수한 도메인네임에 대한 예외처리와 메일 서버 주소로 변환하여 반환
     protected String convertDomainName(String address) {
+        // google로 입력이 들어왔으면 gmail로 변환
         if(address.equals("google.com"))
             return "smtp.gmail.com";
         else
             return "smtp." + address;
     }
 
-    // 메일 전송
-    public boolean sendMail(String userID, String passwd, String from, String to, String content) {
-        String line = "";
-        // @를 기준으로 도메인 이름을 알아낸다.
-        String[] temp = from.split("@");
-        if(temp.length == 2)
-            domainName = temp[1];
-        else {
-            System.out.println("보내는 이메일 주소를 확인해주세요.");
-            return false;
-        }
 
-        try {
-            // 본인 메일 서버 연결
-            if(!connectServer(convertDomainName(domainName), 465))
-                return false;
-        }catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("본인 메일 서버 연결 실패");
-            return false;
-        }
+	// send mail 
+	public boolean sendMail(String userID, String passwd, String to, String Subject, String content) {
+		 String line = "";
+	        // @를 기준으로 도메인 이름을 알아낸다.
+	        String[] temp = userID.split("@");
+	        if(temp.length == 2)
+	            domainName = temp[1];
+	        else {
+	            System.out.println("보내는 이메일 주소를 확인해주세요.");
+	            return false;
+	        }
 
-        // 아이디, 비밀번호 암호화
-        this.id = Base64.getEncoder().encodeToString(userID.getBytes(StandardCharsets.UTF_8));
-        this.password = Base64.getEncoder().encodeToString(passwd.getBytes(StandardCharsets.UTF_8));
+	        try {
+	            // 본인 메일 서버 연결
+	            if(!connectServer(convertDomainName(domainName), 465))
+	                return false;
+	        }catch (IOException e) {
+	            e.printStackTrace();
+	            System.out.println("본인 메일 서버 연결 실패");
+	            return false;
+	        }
 
-        try {
-            /*
-            // HELO 명령 전송, 응답코드 250 확인
-            if(!sendCommand("HELO ", localhost.getHostName(), "250"))
-                return false;*/
+	        // 아이디, 비밀번호 암호화
+	        this.id = Base64.getEncoder().encodeToString(userID.getBytes(StandardCharsets.UTF_8));
+	        this.password = Base64.getEncoder().encodeToString(passwd.getBytes(StandardCharsets.UTF_8));
 
-            // EHLO 명령 전송, 응답코드 250 확인
-            if(!sendCommand("EHLO ", convertDomainName(domainName), "250"))
-                return false;
+		try {
+			/*
+			 * // HELO request send, responsecode 250, if(!sendCommand("HELO ", localhost.getHostName(),
+			 * "250")) return false;
+			 */
 
-            pw.println("AUTH LOGIN");
-            line = br.readLine();
-            while(!line.substring(0,3).equals("334")) {
-                System.out.println(line);
-                line = br.readLine();
-            }
-            pw.println(this.id);
-            System.out.println("response : " + br.readLine());
-            pw.println(this.password);
-            System.out.println("response : " + br.readLine());
+			// EHLO request send, look responsecode is 250
+			if(!sendCommand("EHLO ", convertDomainName(domainName), "250"))
+				return false;
+			
+			pw.println("AUTH LOGIN");
+			line = br.readLine();
+			// the username and password are sent from the client to the server
+			while (!line.substring(0, 3).equals("334")) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+			
+			pw.println(this.id);
+			System.out.println("response : " + br.readLine());
+			pw.println(this.password);
+			System.out.println("response : " + br.readLine());
 
-            // MAIL FROM 명령 전송, 응답코드 250 확인
-            if(!sendCommand("MAIL FROM: ", "<" + from + ">", "250"))
-                return false;
+			// MAIL FROM request send, confirm code is 250
+			if (!sendCommand("MAIL FROM: ", "<" + userID + ">", "250"))
+				return false;
 
-            // RCPT 명령 전송, 응답코드 250 확인
-            if(!sendCommand("RCPT TO: ", "<" + to + ">", "250"))
-                return false;
+			// RCPT request send, confirm code is 250 
+			if (!sendCommand("RCPT TO: ", "<" + to + ">", "250"))
+				return false;
 
-            // DATA 명령 전송, 응답코드 354 확인
-            if(!sendCommand("DATA", "", "354"))
-                return false;
+			// DATA request send, confirm code is 354
+			//354 is Start mail input; end with <CRLF>.<CRLF>
+			if (!sendCommand("DATA", "", "354"))
+				return false;
 
-            // 본문 전송
-            pw.println("From: "+from+" <" + from + ">");
-            pw.println("To: " + to + " <" + to + ">");
-            pw.println("Subject: Testing email from telnet\r\n");
-            pw.println("Content: " + content);
-            // 본문 전송 마무리
-            if(!sendCommand(".", "", "250"))
-                return false;
+			// send mail
+			pw.println("From: <" + userID + ">");
+			pw.println("To: <" + to + ">");
+			pw.println("Subject:"+Subject);
+			pw.println(content);
+			// end the mail
+			if (!sendCommand(".", "", "250"))
+				return false;
 
-        }catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("메일을 보내는 중 오류가 발생했습니다.");
-            return false;
-        }finally {
-            try {
-                pw.println("QUIT");
-                br.close();
-                pw.close();
-                socket.close();
-            }catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return true;
-    }
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("During sending email, error is occur");
+			return false;
+		} finally {
+			try {
+				pw.println("QUIT");
+				br.close();
+				pw.close();
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return true;
+	}
 }
